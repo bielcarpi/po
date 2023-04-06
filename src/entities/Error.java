b65dbc50c09976c312c7103ba18b0bb20b017a70
angel.garcia <angel.garcia@students.salle.url.edu>
2023-04-06 18:08:20 +0200

    Error class implemented

--- src/entities/Error.java
+++ src/entities/Error.java
@@ -1,4 +1,31 @@
 package entities;
 
 public class Error {
+    private ErrorType type;
+    private String message;
+    private int line;
+    private int column;
+
+    public Error(ErrorType type, String message, int line, int column) {
+        this.type = type;
+        this.message = message;
+        this.line = line;
+        this.column = column;
+    }
+
+    public ErrorType getType() {
+        return type;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public int getColumn() {
+        return column;
+    }
 }
--- src/entities/ErrorManager.java
+++ src/entities/ErrorManager.java
@@ -1,4 +1,18 @@
 package entities;
 
 public class ErrorManager {
+    private static ErrorManager instance = null;
+
+    private ErrorManager() {
+    }
+
+    public static ErrorManager getInstance() {
+        if (instance == null) {
+            instance = new ErrorManager();
+        }
+        return instance;
+    }
+
+    public void addError(Error error) {
+    }
 }

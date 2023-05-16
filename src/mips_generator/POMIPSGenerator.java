ad26fae3126d410565d2852f99c5eaddfaa7cc51
angel.garcia <angel.garcia@students.salle.url.edu>
2023-05-16 22:01:11 +0200

    Base of multiply and 2 when we have 2 adresses

--- src/mips_generator/POMIPSGenerator.java
+++ src/mips_generator/POMIPSGenerator.java
@@ -9,7 +9,14 @@ public class POMIPSGenerator implements MIPSGenerator {
 
         @Override
     public void generateMIPS(@NotNull String tac, @NotNull String fileName) {
+        String[] tmp = {"$t0", "$t1", "$t2", "$t3", "$t4", "$t5", "$t6", "$t7", "$t8", "$t9"};
+        int tmpIndex = 0;
+
         try (PrintWriter out = new PrintWriter(fileName)) {
+            out.println(".section .text\n");
+            out.println(".global __start\n");
+            out.println("__start:");
+
             for (String tacLine : tac.split("\n")) {
                 String[] components = tacLine.split(" ");
                 String var = components[0];
@@ -21,28 +28,52 @@ public class POMIPSGenerator implements MIPSGenerator {
 
                     switch (op) {
                         case "+":
-                            out.println("add " + var + ", " + c1 + ", " + c2);
+                            out.println("\tadd " + var + ", " + c1 + ", " + c2);
                             break;
                         case "-":
-                            out.println("sub " + var + ", " + c1 + ", " + c2);
+                            out.println("\tsub " + var + ", " + c1 + ", " + c2);
                             break;
                         case "*":
-                            out.println("mul " + var + ", " + c1 + ", " + c2);
+                            // Check if c1 or c2 is a constant, or if both are constants or if none is a constant
+                            if(c1.matches("-?\\d+") && c2.matches("-?\\d+")) {
+                                out.println("\tli " + tmp[tmpIndex] + ", " + c1);
+                                tmpIndex++;
+
+                                if(tmpIndex < tmp.length -1) {
+                                    tmpIndex++;
+                                } else {
+                                    tmpIndex = 1;
+                                }
+                                out.println("\tli " + tmp[tmpIndex] + ", " + c2);
+                                out.println("\tmul " + tmp[tmpIndex -1] + ", " + tmp[tmpIndex]);
+                            }
+
                             break;
                         case "/":
-                            out.println("div " + var + ", " + c1 + ", " + c2);
+                            out.println("\tdiv " + var + ", " + c1 + ", " + c2);
                             break;
                         case "and":
-                            out.println("and " + var + ", " + c1 + ", " + c2);
+                            out.println("\tand " + var + ", " + c1 + ", " + c2);
                             break;
 
                         case "or":
-                            out.println("or " + var + ", " + c1 + ", " + c2);
+                            out.println("\tor " + var + ", " + c1 + ", " + c2);
                             break;
                     }
                 } else {
-                    out.println("li " + var + ", " + c1);
+                    if(c1.matches("-?\\d+")) {
+                            out.println("\tli " + tmp[tmpIndex] + ", " + c1);
+                    } else {
+                            out.println("\tmove " + tmp[tmpIndex] + ", " + c1);
+                    }
                 }
+
+                if(tmpIndex < tmp.length -1) {
+                    tmpIndex++;
+                } else {
+                    tmpIndex = 0;
+                }
+
             }
         } catch (FileNotFoundException e) {
             e.printStackTrace();
